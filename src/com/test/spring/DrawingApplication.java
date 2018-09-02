ec83d1ecf14fd670da57bc4751006e569ef0bf6d
Ankit Garg <ankit.g.garg@oracle.com>
2018-09-02 11:00:34 +0530

    Initial Commit

---
+++ src/com/test/spring/DrawingApplication.java
@@ -0,0 +1,13 @@
+package com.test.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DrawingApplication {
+
+  public static void main(String[] args) {
+    ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+    Triangle triangle = (Triangle) context.getBean("triangle");
+    triangle.draw();
+  }
+}
